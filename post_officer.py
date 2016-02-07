fe5debb3e6cfb89f3c01d1e58bbfdb068a7c9ad0
theoka <usingalovelytune@gmail.com>
2016-02-07 13:54:20 -0500

    Use getpass, fix sendmail() params

--- post_officer.py
+++ post_officer.py
@@ -2,24 +2,27 @@
 
 import smtplib
 import imapy
+import getpass
 
 def send_some_mail():
     smtpObj = smtplib.SMTP('smtp.gmail.com',587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    username = input('Username: ')
-    password = input('Password: ')
-    recipient = input('Recipient: ')
-    smtpObj.login(username,password)
-    #Body is supposed to be separated by newline, but this has not worked in tests
-    smtpObj.sendmail(username,recipient,'Subject: Hello World \n This is an email.')
+    smtpObj.login(username = input('Username: '),
+                            password = getpass.getpass())
+    #I guess sendmal() needs 2 newlines after the subject??
+    subject = input('Subject: ')
+    body = input('Body: ')
+    smtpObj.sendmail(username,
+                                  recipient = input('Recipient: '),
+                                  'Subject: %s\n\n%s' % (subject,body))
     smtpObj.quit()
 
 def get_some_mail():
     imapObj = imapy.connect(
         host = 'imap.gmail.com',
         username = input('Username: '),
-        password = input('Password: '),
+        password = getpass.getpass(),
         ssl = True)
     folders = imapObj.folders()
     emails = imapObj.folders('Inbox').emails(-5)
